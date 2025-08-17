02fe7dd7c846dfe59496eac247bccf6eb04fe457
Kotov <Kotov.Sasho204@yandex.ru>
2025-08-17 22:24:06 +0300

    add tests and userrepository

--- internal/app/store/testing.go
+++ internal/app/store/testing.go
@@ -15,12 +15,12 @@ func TestStore(t *testing.T, databaseURL string) (*Store, func(...string)) {
 	if err := s.Open(); err != nil {
 		t.Fatal(err)
 	}
-	defer s.Close()
 	return s, func(tables ...string) {
 		if len(tables) > 0 {
 			if _, err := s.db.Exec(fmt.Sprintf("TRUNCATE %s CASCADE", strings.Join(tables, ", "))); err != nil {
 				t.Fatal(err)
 			}
 		}
+		s.Close()
 	}
 }
